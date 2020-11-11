fcea0b5c838b348977a88d78e2e5f1ab78fb6245
Ender <rockers4403@gmail.com>
2020-11-10 21:06:51 -0700

    started to create word possible position finder

--- challenges/word_search.py
+++ challenges/word_search.py
@@ -1,8 +1,8 @@
 import random
 import numpy as np
 
-rows = 25
-cols = 20
+rows = 10
+cols = 10
 
 wordBank = [
     "TEST", "AMUSEMENT", "APPLES", "AUTUMN", "BATS", "BLACK", "BOO", "CANDY", "CAT",
@@ -19,7 +19,8 @@ def getRandomLetter():
 
 def getRandomWord():
     word = wordBank[random.randint(0, len(wordBank) - 1)]
-    return word
+    # return word
+    return "test"
 
 def printMatrix(mat):
     out = ""
@@ -32,55 +33,72 @@ def printMatrix(mat):
 def initMatrix(xLength, yLength):
     return np.zeros((xLength, yLength), dtype=str)
 
-def randLetterMatrix(wordMatrix):
+def randLetterMatrix(wordMatrix):  # Randomizes
     # pos = 0
     for i in range(len(wordMatrix)):
         for j in range(len(wordMatrix[0])):
-            wordMatrix[i][j] = getRandomLetter()
-            # wordMatrix[i][j] = pos
-            # pos += 1
+            if wordMatrix[i][j] == "":
+                wordMatrix[i][j] = getRandomLetter()
+                # wordMatrix[i][j] = pos
+                # pos += 1
     return wordMatrix
 
 
-
-
-def insertWord(wordArray):
+def insertWord(wordMatrix):
     word = getRandomWord()
-    wordStart = 0
-    direction = random.randint(1, 3)  # horizontal: 1, vertical: 2, diagonal: 3
+    direction = 1#random.randint(1, 3)  # horizontal: 1, vertical: 2, diagonal: 3
     if direction == 1:    # Horizontal
         while len(word) > cols:
             word = getRandomWord()
         print(word)
-        rowStart = random.randint(0, len(wordArray) - 1)
-        colStart = random.randint(0, len(wordArray[rowStart]) - len(word))
+        rowStart = random.randint(0, len(wordMatrix) - 1)
+        colStart = random.randint(0, len(wordMatrix[rowStart]) - len(word))
         for letter in word:
-            wordArray[rowStart][wordStart + colStart] = letter
+            wordMatrix[rowStart][colStart] = letter
             colStart += 1
     elif direction == 2:  # Vertical
         while len(word) > rows:
             word = getRandomWord()
         print(word)
-        rowStart = random.randint(0, len(wordArray) - len(word))
-        colStart = random.randint(0, len(wordArray[rowStart]) - 1)
+        rowStart = random.randint(0, len(wordMatrix) - len(word))
+        colStart = random.randint(0, len(wordMatrix[rowStart]) - 1)
         for letter in word:
-            wordArray[wordStart + rowStart][colStart] = letter
+            wordMatrix[rowStart][colStart] = letter
             rowStart += 1
     else:                 # Diagonal
         while len(word) > rows or len(word) > cols:
             word = getRandomWord
         print(word)
-        colStart = random.randint(0, len(wordArray) - len(word))
-        rowStart = random.randint(0, len(wordArray[colStart]) - len(word))
+        colStart = random.randint(0, len(wordMatrix) - len(word))
+        rowStart = random.randint(0, len(wordMatrix[colStart]) - len(word))
         for letter in word:
-            wordArray[wordStart + colStart][wordStart + rowStart] = letter
+            wordMatrix[colStart][rowStart] = letter
             rowStart += 1
             colStart += 1
-    
-    
-# wordList = randLetterMatrix(initMatrix(rows, cols))
+
+def letterCheck(direction, wordMatrix, word):  # tests all possible positions if word will work there ******NEED TO RENAME********
+    checkList = np.ones((rows, cols), dtype=int)
+    if direction == 1:
+        for idxi, i in enumerate(checkList):
+            for idxj, j in enumerate(i):
+                for idxl, letter in enumerate(word):
+                    try:
+                        # print(wordMatrix[idxi][idxj + idxl] + "==" + letter)
+                        if wordMatrix[idxi][idxj + idxl] == "" or wordMatrix[idxi][idxj + idxl] == letter:
+                            pass
+                        else:
+                            checkList[idxi][idxj + idxl] = 0
+                    except IndexError:
+                        checkList[idxi][idxj] = 0
+    return checkList
+
+
+
 wordList = initMatrix(rows, cols)
 
 insertWord(wordList)
+
+printMatrix(letterCheck(1, wordList, getRandomWord()))  # returns one false negative, need to investigate
+wordList = randLetterMatrix(wordList)
 
 printMatrix(wordList)
