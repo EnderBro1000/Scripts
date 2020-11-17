111c397e0427b2ecdc1ce20fec7bdb5a5a06cbe2
Ender <rockers4403@gmail.com>
2020-11-16 20:57:06 -0700

    fixed too many pos options, need to figure out why returning nothing

--- challenges/word_search.py
+++ challenges/word_search.py
@@ -16,11 +16,19 @@ tempWordBank = wordBank
 
 letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-def checkAppend(list, addition):
-    for i in list:
-        if i == addition:
-            return list
-    return list.append(addition)
+
+
+def printMatrix(mat):
+    out = ""
+    for i in range(len(mat)):
+        for j in range(len(mat[0])):
+            out += f"{mat[i][j]} "
+        out += "\n"
+    print(out)
+
+
+def initMatrix(xLength, yLength):
+    return np.zeros((xLength, yLength), dtype=str)
 
 def getRandomLetter():
     # return letters[random.randint(0, len(letters) - 1)]
@@ -33,131 +41,87 @@ def getWord():
         return None
     return word
 
-def printMatrix(mat):
-    out = ""
-    for i in range(len(mat)):
-        for j in range(len(mat[0])):
-            out += f"{mat[i][j]} "
-        out += "\n"
-    print(out)
-
-def initMatrix(xLength, yLength):
-    return np.zeros((xLength, yLength), dtype=str)
-
-def randLetterMatrix(wordMatrix):  # Randomizes
-    # pos = 0
-    for i in range(len(wordMatrix)):
-        for j in range(len(wordMatrix[0])):
-            if wordMatrix[i][j] == "":
-                wordMatrix[i][j] = getRandomLetter()
-                # wordMatrix[i][j] = pos
-                # pos += 1
-    return wordMatrix
-
-def wordCheck(direction, wordMatrix, word):  # tests all possible positions if word will work there
-    possible = False
-    # printMatrix(wordMatrix)
-    checkMatrix = np.ones((rows, cols), dtype=int)
-    for idxi, i in enumerate(checkMatrix):
-        for idxj, j in enumerate(i):
-            for idxl, letter in enumerate(word):
-                if direction == 1:
-                    try:
-                        # print(wordMatrix[idxi][idxj + idxl] + "==" + letter)
-                        if wordMatrix[idxi][idxj + idxl] == "" or wordMatrix[idxi][idxj + idxl] == letter:
-                            possible = True
-                        else:
-                            checkMatrix[idxi][idxj] = 0
-                    except IndexError:
-                        checkMatrix[idxi][idxj] = 0
-                if direction == 2:
-                    try:
-                    # print(wordMatrix[idxi][idxj + idxl] + "==" + letter)
-                        if wordMatrix[idxi + idxl][idxj] == "" or wordMatrix[idxi + idxl][idxj] == letter:
-                            possible = True
-                        else:
-                            checkMatrix[idxi][idxj] = 0
-                    except IndexError:
-                        checkMatrix[idxi][idxj] = 0
-                else:
-                    try:
-                        # print(wordMatrix[idxi][idxj + idxl] + "==" + letter)
-                        if wordMatrix[idxi + idxl][idxj + idxl] == "" or wordMatrix[idxi + idxl][idxj + idxl] == letter:
-                            possible = True
-                        else:
-                            checkMatrix[idxi][idxj] = 0
-                    except IndexError:
-                        checkMatrix[idxi][idxj] = 0
-    return checkMatrix, possible
-
-def wordCheck2(direction, word, wordMatrix):
-    pos = set()
+def randLetterMatrix(wordMatrix):
     for idxi, i in enumerate(wordMatrix):
         for idxj, j in enumerate(i):
+            if wordMatrix[idxi][idxj] == "":
+                wordMatrix[idxi][idxj] = getRandomLetter()
+    return wordMatrix
+
+def wordCheck(direction, word, wordMatrix):
+    checkList = set()
+    for idxi, i in enumerate(wordMatrix):
+        for idxj, j in enumerate(i):
+            possible = True
             for idxl, letter in enumerate(word):
                 if direction == 1:
                     try:
                         if np.all(wordMatrix[idxi][idxj + idxl] != "" and wordMatrix[idxi][idxj + idxl] != letter):
-                            pos.add((idxi, idxj))
+                            possible = False
                     except IndexError:
-                        pos.add((idxi, idxj))
+                        possible = False
+                        break
                 elif direction == 2:
                     try:
                         if np.all(wordMatrix[idxi + idxl][idxj] != "" and wordMatrix[idxi + idxl][idxj] != letter):
-                            pos.add((idxi, idxj))
+                            possible = False
                     except IndexError:
-                        pos.add((idxi, idxj))
+                        possible = False
+                        break
                 else:
                     try:
                         if np.all(wordMatrix[idxi + idxl][idxj + idxl] != "" and wordMatrix[idxi + idxl][idxj + idxl] != letter):
-                            pos.add((idxi, idxj))
+                            possible = False
                     except IndexError:
-                        pos.add((idxi, idxj))
-    return pos
+                        possible = False
+                        break
+            if possible:
+                checkList.add((idxi, idxj))
+    return checkList
+
+def randomDirection(wordMatrix, word):
+    directions = {1, 2, 3}
+    direction = random.sample(directions, 1)
+    direction = direction[0]
+    directions.remove(direction)
+    checkList = wordCheck(direction, wordMatrix, word)
+    print(checkList)
+    while len(checkList) < 1:
+        try:
+            direction = random.sample(directions, 1)
+            direction = direction[0]
+            directions.remove(direction)
+        except ValueError:
+            return None
+        checkList = wordCheck(direction, wordMatrix, word)
+    return direction
 
 def insertWord(wordMatrix):  # I want to create a better system to utilize the checkMatrix, to ensure all words are in if possible
     word = getWord()
     if word is None:
         return wordMatrix
-    directions = list(range(1, 4))
-    random.shuffle(directions)
-    # print(directions)  # horizontal: 1, vertical: 2, diagonal: 3
-    try:
-        direction = directions.pop()
-    except IndexError:
-        return wordMatrix
-    checkList = wordCheck2(direction, wordMatrix, word)
-    for direction in directions:
-        while len(checkList) == 0:
-            try:
-                direction = directions.pop()
-            except IndexError:
-                word = getWord()
-                if word is None:
-                    return wordMatrix
-            checkList = wordCheck2(direction, wordMatrix, word)
-    print(f"direction: {direction}")
+    direction = randomDirection(wordMatrix, word)
+    while direction is None:
+        word = getWord()
+        if word is None:
+            return wordMatrix
+        direction = randomDirection(wordMatrix, word)
+    checkList = wordCheck(direction, wordMatrix, word)
     if direction == 1:    # Horizontal
-        print(f"{checkList} this is insertword")
         pos = random.sample(checkList, 1)
         x, y = pos[0]
-        print(x, y)
         for letter in word:
             wordMatrix[x][y] = letter
             x += 1
     elif direction == 2:  # Vertical
-        print(f"{checkList} this is insertword")
         pos = random.sample(checkList, 1)
         x, y = pos[0]
-        print(x, y)
         for letter in word:
             wordMatrix[x][y] = letter
             y += 1
     else:                 # Diagonal
-        print(f"{checkList} this is insertword")
         pos = random.sample(checkList, 1)
         x, y = pos[0]
-        print(x, y)
         for letter in word:
             wordMatrix[x][y] = letter
             x += 1
@@ -171,9 +135,8 @@ wordList = initMatrix(rows, cols)
 while len(tempWordBank) > 0:
     wordList = insertWord(wordList)
 
-print(wordList)
 wordList = randLetterMatrix(wordList)
 
 printMatrix(wordList)
 
-# There seems to be an infinite loop somewhere, need to find it later
+# print(wordCheck(1, wordBank[0], wordList))
