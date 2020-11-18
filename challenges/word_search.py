14520398dd57a997617248f22e94c952ddbe0f3b
Ender <rockers4403@gmail.com>
2020-11-17 21:09:04 -0700

    unfinished, squashing bugs for wordbank skipping

--- challenges/word_search.py
+++ challenges/word_search.py
@@ -3,10 +3,10 @@ import random
 import numpy as np
 
 rows = 8
-cols = 9
+cols = 8
 
 wordBank = [
-    "AMUSEMENT"#, "AUTUMN", "APPLES", "BLACK", "CANDY", "BATS", "TEST", "BOO", "CAT"#,
+    "AMUSEMENT", "AUTUMN", "APPLES", "BLACK", "CANDY", "BATS", "TEST", "BOO", "CAT"#,
     # "COSTUMES", "DRACULA",	"EERIE"," EXCITEMENT", "FRANKENSTEIN", "FRIGHTEN",
     # "GAMES", "GHOSTS", "GOBLIN", "HALLOWEEN", "HARVEST", "HAYRIDE", "MASK",
     # "MONSTER", "MUMMY", "NIGHT", "OCTOBER", "ORANGE", "PARTY", "PRANK", "PUMPKINS",
@@ -34,12 +34,21 @@ def getRandomLetter():
     # return letters[random.randint(0, len(letters) - 1)]
     return "0"
 
-def getWord():
-    try:
-        word = tempWordBank.pop(0)
-    except IndexError:
+def getWord(wordMatrix):
+    if len(tempWordBank > 1):
+        word = tempWordBank.pop()
+    else:
         return None
-    return word
+    direction, directions = randomDirection(wordMatrix, word)
+
+    if directions < 1:
+        getWord(wordMatrix)
+    possible = 0:
+    while possible < 1:
+        possible = len(wordCheck(direction, word, wordMatrix))
+        direction, directions = randomDirection(wordMatrix, word) #writing HERE ******************************************
+    else: 
+
 
 def randLetterMatrix(wordMatrix):
     for idxi, i in enumerate(wordMatrix):
@@ -84,41 +93,46 @@ def randomDirection(wordMatrix, word):
     direction = random.sample(directions, 1)
     direction = direction[0]
     directions.remove(direction)
-    checkList = wordCheck(direction, wordMatrix, word)
-    print(checkList)
+    checkList = wordCheck(direction, word, wordMatrix)
     while len(checkList) < 1:
         try:
             direction = random.sample(directions, 1)
             direction = direction[0]
             directions.remove(direction)
         except ValueError:
-            return None
-        checkList = wordCheck(direction, wordMatrix, word)
-    return direction
+            return None, 0
+        checkList = wordCheck(direction, word, wordMatrix)
+    return direction, len(directions)
 
 def insertWord(wordMatrix):  # I want to create a better system to utilize the checkMatrix, to ensure all words are in if possible
     word = getWord()
-    if word is None:
-        return wordMatrix
-    direction = randomDirection(wordMatrix, word)
-    while direction is None:
-        word = getWord()
-        if word is None:
+    while word is None:
+        if len(tempWordBank) < 1:
             return wordMatrix
+        word = getWord()
+    direction, directions = randomDirection(wordMatrix, word)
+    while direction is None:
+        while directions < 1:
+            word = getWord()
+            while word is None:
+                if len(tempWordBank) < 1:
+                    return wordMatrix
+            word = getWord()
         direction = randomDirection(wordMatrix, word)
-    checkList = wordCheck(direction, wordMatrix, word)
+        direction = randomDirection(wordMatrix, word)
+    checkList = wordCheck(direction, word, wordMatrix)
     if direction == 1:    # Horizontal
         pos = random.sample(checkList, 1)
         x, y = pos[0]
         for letter in word:
             wordMatrix[x][y] = letter
-            x += 1
+            y += 1
     elif direction == 2:  # Vertical
         pos = random.sample(checkList, 1)
         x, y = pos[0]
         for letter in word:
             wordMatrix[x][y] = letter
-            y += 1
+            x += 1
     else:                 # Diagonal
         pos = random.sample(checkList, 1)
         x, y = pos[0]
